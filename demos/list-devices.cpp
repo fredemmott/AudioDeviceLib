15dce0319aee78d7888661f2455f0db5e1883ae8
Fred Emmott <fred@fredemmott.com>
2021-02-13 10:28:34 -0600

    Add list-devices demo

---
+++ demos/list-devices.cpp
@@ -0,0 +1,45 @@
+/* Copyright (c) 2019-present, Fred Emmott
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file.
+ */
+
+#include "../src/AudioDevices.h"
+
+#include <iostream>
+
+using namespace FredEmmott::Audio;
+using namespace std;
+
+void dump_devices(AudioDeviceDirection dir) {
+  const auto devices = GetAudioDeviceList(dir);
+  for (const auto& [id, device]: devices) {
+    cout << "\"" << device.displayName << "\"" << endl;
+    cout << "\tID:        \"" << id << "\"" << endl;
+    cout << "\tInterface: \"" << device.interfaceName << "\"" << endl;
+    cout << "\tEndpoint:  \"" << device.endpointName << "\"" << endl;
+    cout << "\tState:     ";
+    switch (device.state) {
+      case AudioDeviceState::CONNECTED:
+        cout << "CONNECTED" << endl;
+        break;
+      case AudioDeviceState::DEVICE_NOT_PRESENT:
+        cout << "DEVICE_NOT_PRESENT" << endl;
+        break;
+      case AudioDeviceState::DEVICE_DISABLED:
+        cout << "DEVICE_DISABLED" << endl;
+        break;
+      case AudioDeviceState::DEVICE_PRESENT_NO_CONNECTION:
+        cout << "DEVICE_PRESENT_NO_CONNECTION" << endl;
+        break;
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  cout << "----- INPUT DEVICES -----" << endl;
+  dump_devices(AudioDeviceDirection::INPUT);
+  cout << "----- OUTPUT DEVICES -----" << endl;
+  dump_devices(AudioDeviceDirection::OUTPUT);
+  return 0;
+}
